31c1c232c06a2afb4d96ce42dd94fc6459e7c02f
Xavier John <xavierjohn@hotmail.com>
2017-08-18 11:01:16 -0700

    Convert to .netstandard 2.0

---
+++ ClientCertificateMiddleware/CertificateAuthenticationExtensions.cs
@@ -0,0 +1,17 @@
+using System;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CWiz.ClientCertificateMiddleware
+{
+    public static class CertificateAuthenticationExtensions
+    {
+        public static AuthenticationBuilder AddCertificateAuthentication(this AuthenticationBuilder builder, Action<CertficateAuthenticationOptions> configureOptions)
+        {
+            builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<IPostConfigureOptions<CertficateAuthenticationOptions>, CertificateAuthenticationPostConfigureOptions>());
+            return builder.AddScheme<CertficateAuthenticationOptions, CertificateAuthenticationHandler>(CertificateAuthenticationDefaults.AuthenticationScheme, "Certificate Authentication", configureOptions);
+        }
+    }
+}
